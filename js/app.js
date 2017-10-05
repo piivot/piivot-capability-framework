3de3cd0e234571e25e2d5c56032f449b5220509a
Faizan Sabzaali <fsabzaali@gmail.com>
2017-10-04 20:19:59 -0700

    Added javascript to update content as dropdown or slider changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
-$('#ex4').slider();
+$(".piivot-question").on("change", function(){
+    
+    let sectionId = $(this).attr('piivot-section');
+    let capabilityId = $(this).attr('piivot-capability');
+    let questionId = $(this).attr('id')
+    let contentHolder = "#" + $(this).attr('piivot-question-content');
+    let score = $(this).val();
 
-$('#ex4').on("slide", function(slideevt) {
+    if (score == null)
+    {
+        score = $(this).attr('data-value');
+    }
+
+    let section = $.grep(frameworkInfo.sections, function(s) { return s.id == sectionId;});
+    let capability = $.grep(section[0].capabilities, function(c) { return c.id == capabilityId;});
+    let mainQuestion = $.grep(capability[0].questions, function(e) {return e.id == questionId;});
+    let mainQuestionContent = $.grep(mainQuestion[0].scores, function(e) {return e.value == score;});
+
+    $(contentHolder).text(mainQuestionContent[0].description);    
+});
+
+$('.piivot-slider').slider();
+
+$('.piivot-slider').on("slide", function(slideevt) {
 	$('#ex4SliderVal').text(slideevt.value);
 });
 
-$('#ex5').slider({
+$('#ORC-CL1-05').slider({
     ticks: [1,2,3,4,5],
     ticks_labels: ['1', '2', '3', '4', '5'],
     ticks_snap_bounds: 30
--- js/questions.js
+++ js/questions.js
@@ -1,102 +1,165 @@
 var question1Info = {
-    attributeId = "ORC-CL1-01",
-    title = "Internal Organization Workshops", 
-    scores = [
+    id: "ORC-CL1-01",
+    title: "Internal Organization Workshops", 
+    scores: [
         {
-            value = 1,
-            description = "Score of 1 indicates you have not run any workshops"
+            value: 1,
+            description: "Score of 1 indicates you have not run any workshops"
         },
         {
-            value = 2,
-            description = "Score of 2 indicates you ran one workshop and ran the consultant out"
+            value: 2,
+            description: "Score of 2 indicates you ran one workshop and ran the consultant out"
         },
         {
-            value = 3,
-            description = "Score of 3 indicates you value the internal organization workshop"
+            value: 3,
+            description: "Score of 3 indicates you value the internal organization workshop"
         },
         {
-            value = 4,
-            description = "Score of 4 indicates you run regular workshops"
+            value: 4,
+            description: "Score of 4 indicates you run regular workshops"
         },
         {
-            value = 5,
-            description = "Score of 5 indicates you are the best organization"
+            value: 5,
+            description: "Score of 5 indicates you are the best organization"
         }
     ],
-    comparisonScore = 1,
-    type = "dropdown"
+    comparisonScore: 1,
+    type: "dropdown"
     
 }
 
 var question2Info = {
-    attributeId = "ORC-CL1-02",
-    title = "3rd Party Vendor Educational Workshops/Meetings", 
-    scores = [
+    id: "ORC-CL1-02",
+    title: "3rd Party Vendor Educational Workshops/Meetings", 
+    scores: [
         {
-            value = 1,
-            description = "Score of 1 indicates you have not run any 3rd party workshops"
+            value: 1,
+            description: "Score of 1 indicates you have not run any 3rd party workshops"
         },
         {
-            value = 2,
-            description = "Score of 2 indicates you ran one workshop and ran the 3rd party out"
+            value: 2,
+            description: "Score of 2 indicates you ran one workshop and ran the 3rd party out"
         },
         {
-            value = 3,
-            description = "Score of 3 indicates you value the 3rd party workshops"
+            value: 3,
+            description: "Score of 3 indicates you value the 3rd party workshops"
         },
         {
-            value = 4,
-            description = "Score of 4 indicates you interact regularly with 3rd parties"
+            value: 4,
+            description: "Score of 4 indicates you interact regularly with 3rd parties"
         },
         {
-            value = 5,
-            description = "Score of 5 indicates you and third parties are partners"
+            value: 5,
+            description: "Score of 5 indicates you and third parties are partners"
         }
     ],
-    comparisonScore = 1,
-    type = "dropdown"
+    comparisonScore: 1,
+    type: "dropdown"
     
 }
 
 var question3Info = {
-    attributeId = "ORC-CL1-03",
-    title = "Recognition for the need for Cloud Adoption: <strong>IT Management</strong>", 
-    scores = [
+    id: "ORC-CL1-03",
+    title: "Recognition for the need for Cloud Adoption: <strong>IT Management</strong>", 
+    scores: [
         {
-            value = 1,
-            description = "Score of 1 indicates you have not run any 3rd party workshops"
+            value: 1,
+            description: "Score of 1 indicates you have not run any 3rd party workshops"
         },
         {
-            value = 2,
-            description = "Score of 2 indicates you ran one workshop and ran the 3rd party out"
+            value: 2,
+            description: "Score of 2 indicates you ran one workshop and ran the 3rd party out"
         },
         {
-            value = 3,
-            description = "Score of 3 indicates you value the 3rd party workshops"
+            value: 3,
+            description: "Score of 3 indicates you value the 3rd party workshops"
         },
         {
-            value = 4,
-            description = "Score of 4 indicates you interact regularly with 3rd parties"
+            value: 4,
+            description: "Score of 4 indicates you interact regularly with 3rd parties"
         },
         {
-            value = 5,
-            description = "Score of 5 indicates you and third parties are partners"
+            value: 5,
+            description: "Score of 5 indicates you and third parties are partners"
         }
     ],
-    comparisonScore = 1,
-    type = "dropdown"
-    
+    comparisonScore: 1,
+    type: "dropdown"    
+}
+
+var question4Info = {
+    id: "ORC-CL1-04",
+    title: "Limited Business Unit Leadership/Individual commitment and approvals", 
+    scores: [
+        {
+            value: 1,
+            description: "Score of 1 indicates you have not run any 3rd party workshops"
+        },
+        {
+            value: 2,
+            description: "Score of 2 indicates you ran one workshop and ran the 3rd party out"
+        },
+        {
+            value: 3,
+            description: "Score of 3 indicates you value the 3rd party workshops"
+        },
+        {
+            value: 4,
+            description: "Score of 4 indicates you interact regularly with 3rd parties"
+        },
+        {
+            value: 5,
+            description: "Score of 5 indicates you and third parties are partners"
+        }
+    ],
+    comparisonScore: 1,
+    type: "dropdown"    
+}
+
+var question5Info = {
+    id: "ORC-CL1-05",
+    title: "Limited Business Unit Leadership/Individual commitment and approvals", 
+    scores: [
+        {
+            value: 1,
+            description: "Score of 1 indicates you have not run any 3rd party workshops"
+        },
+        {
+            value: 2,
+            description: "Score of 2 indicates you ran one workshop and ran the 3rd party out"
+        },
+        {
+            value: 3,
+            description: "Score of 3 indicates you value the 3rd party workshops"
+        },
+        {
+            value: 4,
+            description: "Score of 4 indicates you interact regularly with 3rd parties"
+        },
+        {
+            value: 5,
+            description: "Score of 5 indicates you and third parties are partners"
+        }
+    ],
+    comparisonScore: 1,
+    type: "dropdown"    
 }
 
 var frameworkInfo = {
-    sections = [
+    sections: [
         {
-            name = "Organizational Capability",
-            capabilities = [
+            name: "Organizational Capability",
+            id: "S01-ORG",
+            capabilities: [
                 {
-                    name = "CL1",
-                    description = "Recognition of need",
-                    questions = [question1Info, question2Info, question3Info]
+                    id: "CL1",
+                    description: "Recognition of need",
+                    questions: [question1Info, question2Info, question3Info]
+                },
+                {
+                    id: "CL2",
+                    description: "Recognition of need",
+                    questions: [question4Info, question5Info]
                 }
             ]
         }
