6b48eb73a2adada451ac6bec168ae4a4fcfe2706
Faizan Sabzaali <fsabzaali@gmail.com>
2017-10-05 19:22:54 -0700

    updated chart info based on initial calculation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var overallScore = [];
+
 $(".piivot-question").on("change", function(){
     Pizza.init();
     let sectionId = $(this).attr('piivot-section');
@@ -14,16 +16,12 @@ $(".piivot-question").on("change", function(){
     let section = $.grep(frameworkInfo.sections, function(s) { return s.id == sectionId;});
     let capability = $.grep(section[0].capabilities, function(c) { return c.id == capabilityId;});
     let mainQuestion = $.grep(capability[0].questions, function(e) {return e.id == questionId;});
+    let mainQuestionComparisonScore = mainQuestion[0].comparisonScore;
     let mainQuestionContent = $.grep(mainQuestion[0].scores, function(e) {return e.value == score;});
 
     $(contentHolder).text(mainQuestionContent[0].description);    
 
-    y = parseInt(score) + 1;
-    x = parseInt(score) + 1;
-
-    myChart.data.datasets[0].data[0].x = x;
-    myChart.data.datasets[0].data[0].y = y;
-    myChart.update();
+    updateScore(questionId,score,mainQuestionComparisonScore);
 
     /*$('#graphinfoyourcapability').attr('data-y',y);
     $('#graphinfoyourcapability').attr('data-x',x);    
@@ -46,6 +44,35 @@ $('#ORC-CL1-05').slider({
     ticks_labels: ['1', '2', '3', '4', '5'],
     ticks_snap_bounds: 30
 });
+
+function updateScore(id,score,comparisonScore)
+{
+    let currentQuestion = $.grep(overallScore, function(s) { return s.id == id;});
+    
+    if (currentQuestion != null && currentQuestion.length > 0)
+    {
+        currentQuestion[0].score = score * comparisonScore;
+    }
+    else
+    {
+        let newscore = {};
+        newscore.id = id;
+        newscore.score = score * comparisonScore;
+        overallScore.push(newscore);
+    }
+
+    let chartScore = 0.0;
+    $.each(overallScore, function(){
+        chartScore += this.score;
+    })
+
+    if (chartScore > 20) chartScore = 20;
+
+    myChart.data.datasets[0].data[0].x = chartScore;
+    myChart.data.datasets[0].data[0].y = chartScore;
+    myChart.update();
+
+}
 
 //Pizza.init();
 
