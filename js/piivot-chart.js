f0538001b1c6a187b5eee0b2651386dd23c79798
Faizan Sabzaali <fsabzaali@gmail.com>
2017-10-17 20:28:23 -0700

    Updated charts

--- js/piivot-chart.js
+++ js/piivot-chart.js
@@ -1,6 +1,6 @@
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'horizontalBar',
+    type: 'horizontalBar',    
     data: {
         labels: ["Organizational", "Operational", "Governance", "Platform Selection", "Identity and Access Management", "Network Elasticity", "Security Adherance", "Data Management"],
         datasets: [{
@@ -13,7 +13,19 @@ var myChart = new Chart(ctx, {
             ]*/,            
             pointBackgroundColor:["black"],
             borderWidth: 1,
-            pointRadius: 10
+            pointRadius: 10,
+            datalabels: {
+                color: 'white',
+                display: function(context) {
+                    return context.dataset.data[context.dataIndex] > 20;
+                },
+                font: {
+                    weight: 'bold'
+                },
+                formatter: function(value, context) {
+                    return Math.round(value) + '%';
+                }
+            }
         },
         {
             label: 'Ideal Capability',
@@ -25,12 +37,15 @@ var myChart = new Chart(ctx, {
             ]*/,            
             pointBackgroundColor:["#f69320"],
             borderWidth: 1,
-            pointRadius: 10
-        }
+            pointRadius: 10,
+            datalabels: {
+                display: false
+            }                
+        }        
         ]
     },
     options: {
-        showLines: false,
+        showLines: false,        
         scales: {
             yAxes: [{
                 stacked: true,
